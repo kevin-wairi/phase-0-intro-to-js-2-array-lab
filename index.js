36208e4db1ca91a10d56df63e82cdcd0fae96894
kevin Wairi <kevinwairi@gmail.com>
2023-03-11 16:14:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);   
+}
+
+function destructivelyRemoveLastCat(name){
+    return  cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    return  cats.shift(name);
+}
+
+function appendCat(name){
+    return  cats.slice(0,name);
+}
+
+function appendCat(name){
+    let copyName = [...cats,name];
+       return copyName;
+  }
+  
+  function prependCat(name){
+    let copyName = [name,...cats];
+       return copyName;
+  }
+  
+function removeLastCat(){
+    return cats.slice(0,cats.length-1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
